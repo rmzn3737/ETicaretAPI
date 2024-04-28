ef283a6f33b10675436916b69bd9c8c512d022f0
ramazan <eserramazan43@gmail.com>
2024-04-28 13:10:19 +0300

    7. ders sonu. Mini E-Ticaret | EF Core Tracking Performans Optimizasyonu yapıldı.

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -22,13 +22,18 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-           await _productWriteRepository.AddRangeAsync(new()
-            {
-                new(){ Id=Guid.NewGuid(),Name="Product 1",Price=100,CreatedDate =DateTime.UtcNow,Stock=10 },
-                new(){ Id=Guid.NewGuid(),Name="Product 2",Price=200,CreatedDate =DateTime.UtcNow,Stock=20 },
-                new(){ Id=Guid.NewGuid(),Name="Product 3",Price=300,CreatedDate =DateTime.UtcNow,Stock=30 },
-            });
-           var count = await _productWriteRepository.SaveAsync();
+            //await _productWriteRepository.AddRangeAsync(new()
+            // {
+            //     new(){ Id=Guid.NewGuid(),Name="Product 1",Price=100,CreatedDate =DateTime.UtcNow,Stock=10 },
+            //     new(){ Id=Guid.NewGuid(),Name="Product 2",Price=200,CreatedDate =DateTime.UtcNow,Stock=20 },
+            //     new(){ Id=Guid.NewGuid(),Name="Product 3",Price=300,CreatedDate =DateTime.UtcNow,Stock=30 },
+            // });
+            //var count = await _productWriteRepository.SaveAsync();
+
+            Product eldeEdilenProduct= await _productReadRepository.GetByIdAsync("c79b3cf9-959c-4458-94d4-edb1499a7d0b",false);
+            //eldeEdilenProduct.Name = "Ahmet";
+            eldeEdilenProduct.Name = "Mehmet";
+            await _productWriteRepository.SaveAsync();
         }
         
         [HttpGet("{id}")]
