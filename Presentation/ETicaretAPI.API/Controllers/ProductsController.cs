d9379432ef0b486a34950eb53e5802b3223dd1de
ramazan <eserramazan43@gmail.com>
2024-04-28 17:02:09 +0300

    8. Dersin sonu,sorunsuz çalışıyor.
    Mini E-Ticaret | SaveChangeAsync Interceptor

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -15,27 +15,38 @@ namespace ETicaretAPI.API.Controllers
         readonly private IProductReadRepository _productReadRepository;
         readonly private IOrderWriteRepository _orderWriteRepository;
         readonly private ICustomerWriteRepository _customerWriteRepository;
+        private readonly ICustomerReadRepository _customerReadRepository;
+        private readonly IOrderReadRepository _orderReadRepository;
 
         public ProductsController(
             IProductWriteRepository productWriteRepository,
             IProductReadRepository productReadRepository,
             IOrderWriteRepository orderWriteRepository,
-            ICustomerWriteRepository customerWriteRepository)
+            ICustomerWriteRepository customerWriteRepository,
+            ICustomerReadRepository customerReadRepository,
+            IOrderReadRepository orderReadRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
             _orderWriteRepository = orderWriteRepository;
             _customerWriteRepository = customerWriteRepository;
+            _customerReadRepository = customerReadRepository;
+            _orderReadRepository = orderReadRepository;
         }
         [HttpGet]
         public async Task Get()
         {
-            var customerId = Guid.NewGuid();
+
+            Order order = await _orderReadRepository.GetByIdAsync("c0b1fd8c-61e0-4010-9478-067a00c072d7");
+            order.Address = "Türkiye, İstanbul";
+            await _orderWriteRepository.SaveAsync();
+            
+            /*var customerId = Guid.NewGuid();
             await _customerWriteRepository.AddAsync(new() { Id = customerId,Name="Alex De Souza" });
 
            await _orderWriteRepository.AddAsync(new() { Description = "bla bla bla", Address = "Türkiye, Konya",CustomerID=customerId });
            await _orderWriteRepository.AddAsync(new() { Description = "bla2 bla2 bla2", Address = "Türkiye, Ege",CustomerID = customerId });
-           await _orderWriteRepository.SaveAsync();
+           await _orderWriteRepository.SaveAsync();*/
         }
         
         
