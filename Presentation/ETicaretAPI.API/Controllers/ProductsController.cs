3b3a0a28a6b6836d3c8c6a35475a9e445ec014ac
ramazan <eserramazan43@gmail.com>
2024-05-05 01:08:58 +0300

    #18- Asp.NET Core 6 + Angular İle Mini E-Ticaret _ Custom HttpClient Servisi Oluşturmak. Bu dersin 26.50 dksında beacend bölümünden frontende geçtiğimiz yere kadar.

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 ﻿using ETicaret.Application.Abstractions;
 using ETicaret.Application.Repositories;
+using ETicaret.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using ETicaretAPI.Persistence.Concretes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace ETicaretAPI.API.Controllers
 {
@@ -13,44 +15,60 @@ namespace ETicaretAPI.API.Controllers
     {
         readonly private IProductWriteRepository _productWriteRepository;//private readonly IProductService _productService;
         readonly private IProductReadRepository _productReadRepository;
-        readonly private IOrderWriteRepository _orderWriteRepository;
-        readonly private ICustomerWriteRepository _customerWriteRepository;
-        private readonly ICustomerReadRepository _customerReadRepository;
-        private readonly IOrderReadRepository _orderReadRepository;
+        
 
         public ProductsController(
             IProductWriteRepository productWriteRepository,
-            IProductReadRepository productReadRepository,
-            IOrderWriteRepository orderWriteRepository,
-            ICustomerWriteRepository customerWriteRepository,
-            ICustomerReadRepository customerReadRepository,
-            IOrderReadRepository orderReadRepository)
+            IProductReadRepository productReadRepository)
         {
             _productWriteRepository = productWriteRepository;
             _productReadRepository = productReadRepository;
-            _orderWriteRepository = orderWriteRepository;
-            _customerWriteRepository = customerWriteRepository;
-            _customerReadRepository = customerReadRepository;
-            _orderReadRepository = orderReadRepository;
+            
         }
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-
-            //Order order = await _orderReadRepository.GetByIdAsync("c0b1fd8c-61e0-4010-9478-067a00c072d7");
-            //order.Address = "Türkiye, İstanbul";
-            //await _orderWriteRepository.SaveAsync();
-
-            return Ok("Merhaba");
-            
-            /*var customerId = Guid.NewGuid();
-            await _customerWriteRepository.AddAsync(new() { Id = customerId,Name="Alex De Souza" });
-
-           await _orderWriteRepository.AddAsync(new() { Description = "bla bla bla", Address = "Türkiye, Konya",CustomerID=customerId });
-           await _orderWriteRepository.AddAsync(new() { Description = "bla2 bla2 bla2", Address = "Türkiye, Ege",CustomerID = customerId });
-           await _orderWriteRepository.SaveAsync();*/
+            return Ok(_productReadRepository.GetAll(false));
         }
-        
+        [HttpGet("{id}")]
+
+        public async Task <IActionResult > Get(string id)
+        {
+            
+            return Ok(await _productReadRepository.GetByIdAsync(id,false));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Stock = model.Stock,
+                Price = model.Price,
+            });
+
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model) //Update
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Stock = model.Stock;
+            product.Price = model.Price;
+            product.Name    = model.Name;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task <IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return  Ok();
+        }
         
     }
 }
