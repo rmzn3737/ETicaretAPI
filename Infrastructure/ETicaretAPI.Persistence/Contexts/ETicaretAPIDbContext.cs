39e3a8e42328b6edd44cd40f1ad7b0a7257a7b05
ramazan <eserramazan43@gmail.com>
2024-05-06 03:05:35 +0300

    #19- Asp.NET Core 6 + Angular İle Mini E-Ticaret _ Ürün Ekleme İşlemleri - ProductServce Bu videonun 24.00 dksında browserda swagger açılışını durdurmayı anlattı hoca, ben de kapattım, tekrar açmak için aynı bölümü izlemeyi unutmayalım.

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -29,6 +29,7 @@ namespace ETicaretAPI.Persistence.Contexts
                 {
                     EntityState.Added=>data.Entity.CreatedDate=DateTime.UtcNow,
                     EntityState.Modified =>data.Entity.UpdatedDate=DateTime.UtcNow,
+                    _=>DateTime.UtcNow
                 };
             }
             return await base.SaveChangesAsync(cancellationToken);
