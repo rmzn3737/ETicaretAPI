2ca66502e4e8af1c3fea3bb5e449ce46ac662292
ramazan <eserramazan43@gmail.com>
2024-05-12 17:33:04 +0300

    #28- Asp.NET Core 6 + Angular İle Mini E-Ticaret _ Dosya İsimlerinin SEO Uyumlu Düzeltilmesi. Bu dersin 35.00 dksına kadar yaptık ancak kodlar hata veriyor, ürün isimlendireme ve eklemede hata var, tekrar gözden geçireceğiz.

--- Core/ETicaret.Application/Services/IFileService.cs
+++ Core/ETicaret.Application/Services/IFileService.cs
@@ -10,7 +10,6 @@ namespace ETicaret.Application.Services
     public interface IFileService
     {
         Task <List<(string fileName, string path)>>UploadAsync(string path,IFormFileCollection files);//Kayıt başarılıysa <(string fileName, string path)> bize dosya ismi dosya yolunu döndürecek.
-        Task<string> FileRenameAsync(string fileName);
         Task<bool> CopyFileAsync(string path,IFormFile file);
     }
 }
--- Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaret.Application.Services;
+using ETicaretAPI.Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
@@ -19,36 +20,75 @@ namespace ETicaretAPI.Infrastructure.Services
 
         public async Task<bool> CopyFileAsync(string path, IFormFile file)
         {
-            try 
+            try
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
                 return true;
             }
-            catch (Exception ex) 
-            { 
+            catch (Exception ex)
+            {
                 //todo log!
                 throw ex;
             }
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+        private async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
         {
-            throw new NotImplementedException();
+            string newFileName = await Task.Run<string>(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+
+                string newFileName = string.Empty;
+                if (first)
+                {
+                    string oldName = Path.GetFileNameWithoutExtension(fileName);
+                    newFileName = $"{NameOperation.CharacterRegulatory(oldName)} {extension}";
+                }
+                else
+                {
+                    newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf("-");
+                    if (indexNo1 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2 {extension}";
+                    }else
+                    {
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo= newFileName.Substring(indexNo1, indexNo2-indexNo1 -1);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+
+                        newFileName = newFileName
+                        .Remove(indexNo1, indexNo2 - indexNo1 - 1)
+                        .Insert (indexNo1, _fileNo.ToString());
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))//todo bu isimde dosya var mı diye kontrol yapıyoruz.
+                    return await FileRenameAsync(path, newFileName, false);
+                //$"{Path.GetFileNameWithoutExtension(newFileName)}-2
+                else
+                    return newFileName;
+            });
+
+            return newFileName;
+
         }
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
         {
-            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath,path);
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
             if (!Directory.Exists(uploadPath)) Directory.CreateDirectory(uploadPath);
 
             List<(string fileName, string path)> datas = new();
             List<bool> results = new();
-            foreach (IFormFile file in files) 
+            foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
-                bool result= await CopyFileAsync($"{uploadPath}\\{fileNewName}",file);
+                string fileNewName = await FileRenameAsync(uploadPath,file.FileName);
+
+                bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
                 results.Add(result);
             }
