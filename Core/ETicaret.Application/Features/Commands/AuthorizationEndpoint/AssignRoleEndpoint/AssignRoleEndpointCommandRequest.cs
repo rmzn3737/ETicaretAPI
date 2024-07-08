96bc24c1ce7a528b516b0e91f3d32cbee67428e8
rmznsr <eserramazan43@gmail.com>
2024-07-09 00:06:10 +0300

    #66- Asp.NET Core 6 + Angular İle Mini E-Ticaret _ Endpoint'lerle Rollerin İlişkilendirilmesi. Bu videonun 45.00 dksına kadar bitti, migration basmadık henüz. Devam edeceğiz inşallah.

--- Core/ETicaret.Application/Features/Commands/AuthorizationEndpoint/AssignRoleEndpoint/AssignRoleEndpointCommandRequest.cs
+++ Core/ETicaret.Application/Features/Commands/AuthorizationEndpoint/AssignRoleEndpoint/AssignRoleEndpointCommandRequest.cs
@@ -4,4 +4,8 @@ namespace ETicaret.Application.Features.Commands.AuthorizationEndpoint.AssignRol
 
 public class AssignRoleEndpointCommandRequest:IRequest<AssignRoleEndpointCommandResponse>
 {
+    public string[] Roles { get; set; }
+    public string Code { get; set; }
+    public string Menu { get; set; }
+    public Type? Type { get; set; }
 }
